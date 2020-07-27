742f82ee0aa4cb011930306e4b00c43b4f91b8f0
davidyu7 <david.yu@yale.edu>
2020-07-27 13:28:00 -0400

    added comment form

--- src/index.js
+++ src/index.js
@@ -2,20 +2,46 @@ const URL = "http://localhost:3000"
 
 const homeContainer = document.querySelector("#home-content-container")
 const activeContainer = document.querySelector("#active-content-container")
+const commentForm = document.querySelector("#comment-form")
 
 document.querySelector("#navlink-home").addEventListener("click", showHome)
 document.querySelector("#navlink-folders").addEventListener("click", showFolders)
 document.querySelector("#navlink-categories").addEventListener("click", showCategories)
 document.querySelector("#navlink-tags").addEventListener("click", showTags)
 
+commentForm.addEventListener("submit", e => {
+    e.preventDefault()
+
+    fetch(`${URL}/comments`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            project_id: e.target[0].value,
+            email: e.target[2].value,
+            display_name: e.target[3].value,
+            content: e.target[1].value
+        })
+    })
+    .then(() => displayProject(e.target[0].value))
+})
+
+function resetPage() {
+    activeContainer.innerHTML = ''
+    homeContainer.style.display = "none"
+    commentForm.style.display = "none"
+}
+
 function showHome() {
-    activeContainer.innerHTML = ""
+    resetPage()
+
     homeContainer.style.display = "block"
 }
 
 function showFolders() {
-    homeContainer.style.display = "none"
-    activeContainer.innerHTML = ""
+    resetPage()
 
     const displayTitle = document.createElement("h1")
     displayTitle.textContent = "Projects by Role"
@@ -31,8 +57,7 @@ function showFolders() {
 }
 
 function showCategories() {
-    homeContainer.style.display = "none"
-    activeContainer.innerHTML = ""
+    resetPage()
 
     const displayTitle = document.createElement("h1")
     displayTitle.textContent = "Projects by Category"
@@ -49,8 +74,7 @@ function showCategories() {
 }
 
 function showTags() {
-    homeContainer.style.display = "none"
-    activeContainer.innerHTML = ""
+    resetPage()
 
     const displayTitle = document.createElement("h1")
     displayTitle.textContent = "Projects by Tag"
@@ -91,6 +115,9 @@ function displayPreview(project) {
 
     const h3 = document.createElement("h3")
     h3.textContent = project.title
+    h3.addEventListener("click", function() {
+        displayProject(project.id)
+    })
     
     const p = document.createElement("p")
     p.textContent = project.description
@@ -98,4 +125,65 @@ function displayPreview(project) {
     container.append(h3, p)
 
     return container
+}
+
+function displayProject(id) {
+    resetPage()
+    commentForm.style.display = "block"
+    commentForm.reset()
+
+    fetch(`${URL}/projects/${id}`)
+    .then(response => response.json())
+    .then(project => {
+
+        const h4 = document.createElement("h4")
+        const categoryLink = document.createElement("a")
+        categoryLink.textContent = project.category.name
+        const folderLink = document.createElement("a")
+        folderLink.textContent = project.folder.name
+        h4.append(categoryLink, " / ", folderLink)
+
+        const h1 = document.createElement("h1")
+        h1.textContent = project.title
+
+        const p = document.createElement("p")
+        p.textContent = project.description
+
+        const tagsContainer = document.createElement("h4")
+        project.tags.forEach(tag => {
+            const tagLink = document.createElement("a")
+            tagLink.textContent = `#${tag.name}`
+            tagsContainer.append(tagLink, " ")
+        })
+
+        const imagesContainer = document.createElement("div")
+        project.images.forEach(image => {
+            const imageContainer = document.createElement("div")
+            const img = document.createElement("img")
+            img.src = image.src_url
+            imageContainer.appendChild(img)
+            imagesContainer.appendChild(imageContainer)
+        })
+
+        const commentsContainer = document.createElement("div")
+        const commentTitle = document.createElement("h1")
+        commentTitle.innerText = "Comments"
+        commentsContainer.appendChild(commentTitle)
+        project.comments.forEach(comment => {
+            const commentContainer = document.createElement("div")
+            const displayName = document.createElement("h4")
+            displayName.innerText = comment.display_name
+            const commentContent = document.createElement("p")
+            commentContent.innerText = comment.content
+
+            commentContainer.append(displayName, commentContent)
+            
+            commentsContainer.appendChild(commentContainer)
+        })
+
+        activeContainer.append(h4, h1, p, tagsContainer, imagesContainer, commentsContainer)
+
+        commentForm.querySelector("#form-project-id").value = project.id
+
+    })
 }
